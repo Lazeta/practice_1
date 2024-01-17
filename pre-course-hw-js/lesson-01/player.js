77eb152cb5e58f2a64742622d6a45b1d889245fb
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-01-17 19:45:56 +0300

    created object playlist

---
+++ pre-course-hw-js/lesson-01/player.js
@@ -0,0 +1,38 @@
+let playlist = {
+    title: 'Hip-Hop Hits',
+    coverImageUrl: './hip-hop_hits.jpg',
+    info: {
+        totalTracksCount: 4,
+        totalTracksDurationInSeconds: 733
+    },
+    tracks: [
+        {
+            atristName: 'Eminem',
+            title: 'Rap God',
+            // fileUrl: 'Eminem_-_Rap_God.mp3',
+            fileUrl: 'https://dl01.dtmp3.pw/mp3/10893.mp3',
+            isHot: false,
+        },
+        {
+            atristName: '50cent',
+            title: 'In da Club',
+            // fileUrl: '50_Cent_-_In_Da_Club_FlexyOkay.com.mp3',
+            fileUrl: 'https://dl03.dtmp3.pw/mp3/16308.mp3',
+            isHot: true,
+        },
+        {
+            atristName: 'DMX',
+            title: 'X Gon` Give It To Ya',
+            // fileUrl: 'DMX_-_X_Gon`_Give_It_To_Ya.mp3',
+            fileUrl: 'https://dl03.dtmp3.pw/mp3/20615.mp3',
+            isHot: false,
+        },
+        {
+            atristName: 'Eminem feat 50 Cent, Cashis, Lloyd Banks',
+            title: 'You Don`t Know',
+            // fileUrl: 'Eminem_feat_50_Cent_-_Cashis_-_Lloyd_Banks_-_You_Don`t_Know.mp3',
+            fileUrl: 'https://dl03.dtmp3.pw/mp3/14765.mp3',
+            isHot: false,
+        }
+    ]
+}
