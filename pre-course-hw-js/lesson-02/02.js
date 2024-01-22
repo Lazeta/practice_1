e1e3c57b58bb0dd0f54248458f3d566d49615f99
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-01-22 18:15:33 +0300

    added example

---
+++ pre-course-hw-js/lesson-02/02.js
@@ -0,0 +1,15 @@
+for (let i = 1; i <= 10; i++){
+    console.log(i)
+}
+
+let numbers = [1, 2, 3, 4, 5]; 
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i]
+    console.log(number);
+}
+
+let objects = [{name: 'Alice '}, {name: 'Bob'}, {name: 'Charlie'}];
+for (let i = 0; i < objects.length; i++) {
+    const object = objects[i]
+    console.log(object.name);
+}
